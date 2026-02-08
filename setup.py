93de13fbd98040d0f3a18058ce8cb69fbcb73e6f
Humair Munir <humairmunirawan@gmail.com>
2026-02-08 20:43:14 +0500

    feat: add setup.py to make package pip-installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enc",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchaudio",
+        "safetensors",
+        "numpy",
+        "huggingface_hub",
+        "soundfile",
+    ],
+    description="GPU-Optimized Audio Encoder for Bitwav",
+    author="Humair Munir",
+    url="https://github.com/humair-m/enc",
+)
